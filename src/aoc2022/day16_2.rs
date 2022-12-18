b1141042e248e8a6a02e07b4daea27f3abd9f02b
Andreas Daamen <andreas_daamen@web.de>
2022-12-18 13:47:07 +0100

    day16 i give up

--- src/aoc2022/day16_2.rs
+++ src/aoc2022/day16_2.rs
@@ -1,101 +1,54 @@
-use std::{
-    collections::{BTreeMap, HashMap},
-    fmt::Display,
-};
+use std::collections::HashMap;
 
 pub fn tasks() {
     let input = super::load_input(16);
     let data = parse_input(&input);
 
     let graph = transform(data);
+    let initial = PruneGraph::prune_from(&graph, 0, 0, graph.get_next(&0), 1);
     let graph = PruneGraph::from_graph(graph);
 
-    // println!(
-    //     "Part 1 lazy:   {:0>4}",
-    //     dfs::<false>(&mut HashMap::new(), &graph, 0, 0, 30, 0)
-    // );
-    // println!(
-    //     "Part 1 greedy: {:0>4}",
-    //     dfs::<true>(&mut HashMap::new(), &graph, 0, 0, 30, 0)
-    // );
+    let mut best_greed = Score::MIN;
+    let mut best = Score::MIN;
+    for (start, cost) in &initial {
+        let res_greed = dfs::<true>(
+            &mut HashMap::new(),
+            &graph,
+            *start,
+            0,
+            30 - *cost as Minute,
+            0,
+        );
+        let res = dfs::<false>(
+            &mut HashMap::new(),
+            &graph,
+            *start,
+            0,
+            30 - *cost as Minute,
+            0,
+        );
+        println!("Part 1:        {:0>4}", res);
+        println!("Part 1 greedy: {:0>4}", res_greed);
+        best = best.max(res);
+        best_greed = best.max(res_greed);
+    }
+    println!("Part 1 best (greedy) = {best} ({best_greed})");
 
-    // println!(
-    //     "Part 2 lazy:   {:0>4}",
-    //     dfs_2::<false>(&mut HashMap::new(), &graph, 0, 0, 0, 26)
-    // );
-    // println!(
-    //     "Part 2 greedy: {:0>4}",
-    //     dfs_2::<true>(&mut HashMap::new(), &graph, 0, 0, 0, 26)
-    // );
+    let mut best = Score::MIN;
+    for i in 0..initial.len() {
+        let h = initial[i];
+        let e = initial[(i + 1) % initial.len()];
+        let h = (h.0, 26 - h.1 as Minute);
+        let e = (e.0, 26 - e.1 as Minute);
+        let res = dfs_2(&mut HashMap::new(), &graph, h, e, 0, 26);
+        println!("With start {h:?}, {e:?} => {res}");
+        best = best.max(res);
+    }
 
-    let rec = rec(&graph, &mut HashMap::new(), (0, 26), (0, 26), 0);
-    println!("rec {}", rec);
+    println!("Part 2 best = {best}");
 
     // 2501 too low
     // 3000 too high
-}
-
-fn rec(
-    graph: &dyn Graph,
-    cache: &mut HashMap<((Node, Minute), (Node, Minute), ValveMask), Score>,
-    h: (Node, Minute),
-    e: (Node, Minute),
-    open: ValveMask,
-) -> Score {
-    let sort_best = |node: Node, opened: ValveMask| -> Vec<&(Node, u8)> {
-        let mut next = graph
-            .get_next(&node)
-            .iter()
-            .filter(|n| *graph.get_valve(&n.0) & opened == 0)
-            .collect::<Vec<_>>();
-        next.sort_by_key(|n| {
-            let this = graph.get_valve(&n.0);
-            let rhs = n.1 as u64;
-            let d = this / rhs;
-            let r = this % rhs;
-            if r > 0 && rhs > 0 {
-                d + 1
-            } else {
-                d
-            }
-        });
-        next
-    };
-
-    if let Some(cached) = cache.get(&(h, e, open)) {
-        return *cached;
-    }
-
-    let mut best = Score::MIN;
-    for turn_h in sort_best(h.0, open) {
-        if h.1 < turn_h.1 as Minute {
-            continue;
-        }
-
-        let new_h = (turn_h.0, h.1.saturating_sub(1 + turn_h.1 as Minute));
-        let score_h = new_h.1 * graph.get_flow(&turn_h.0) as Score;
-        let open_h = open | graph.get_valve(&turn_h.0);
-
-        let mut best_inner = Score::MIN;
-
-        for turn_e in sort_best(e.0, open) {
-            if e.1 < turn_e.1 as Minute {
-                continue;
-            }
-            let new_e = (turn_e.0, e.1.saturating_sub(1 + turn_e.1 as Minute));
-            let score_e = new_e.1 * graph.get_flow(&turn_e.0) as Minute;
-            let mut open_e = open | graph.get_valve(&turn_e.0);
-            if h.1 < new_e.1 {
-                open_e |= open_h;
-            }
-            let res = rec(graph, cache, new_h, new_e, open_e);
-            best_inner = best_inner.max(res + score_e);
-        }
-        best = best.max(best_inner + score_h);
-    }
-
-    cache.insert((h, e, open), best);
-    best
 }
 
 type Node = usize;
@@ -104,133 +57,91 @@ type ValveMask = Valve;
 type Score = u16;
 type Minute = u16;
 
-fn dfs_2<const GREEDY: bool>(
-    cache: &mut HashMap<(Node, Node, ValveMask, Minute), Score>,
+fn dfs_2(
+    cache: &mut HashMap<((Node, Minute), (Node, Minute), ValveMask, Minute), Score>,
     graph: &dyn Graph,
-    h: Node,
-    e: Node,
+    h: (Node, Minute),
+    e: (Node, Minute),
     opened: ValveMask,
     minute: Minute,
 ) -> Score {
-    if minute == 0 || graph.all_open(&opened) {
-        return 0;
-    }
-
     if let Some(cached) = cache.get(&(h, e, opened, minute)) {
         return *cached;
     }
 
-    let mut best = Score::MIN;
-
-    let h_valve = *graph.get_valve(&h);
-    let h_flow = graph.get_flow(&h) as Score;
-
-    let e_valve = *graph.get_valve(&e);
-    let e_flow = graph.get_flow(&e) as Score;
-
-    // can valve be opened? pos flow and currently closed
-    let h_open = h_valve & opened == 0 && h_flow > 0;
-    let e_open = h != e && e_valve & opened == 0 && e_flow > 0;
-
-    match (h_open, e_open) {
-        (true, true) => {
-            let res = dfs_2::<GREEDY>(
-                cache,
-                graph,
-                h,
-                e,
-                opened | h_valve | e_valve,
-                minute.saturating_sub(1),
-            );
-            best = best.max(res + (minute as Score - 1) * (h_flow + e_flow));
-
-            if !GREEDY {
-                {
-                    for &(e_child, e_cost) in graph.get_next(&e) {
-                        let res = dfs_2::<GREEDY>(
-                            cache,
-                            graph,
-                            h,
-                            e_child,
-                            opened | h_valve,
-                            minute.saturating_sub(e_cost as Minute),
-                        );
-                        best = best.max(res + (h_flow * (minute.saturating_sub(1))));
-                    }
-                }
-                {
-                    for &(h_child, h_cost) in graph.get_next(&h) {
-                        let sub = dfs_2::<GREEDY>(
-                            cache,
-                            graph,
-                            h_child,
-                            e,
-                            opened | e_valve,
-                            minute.saturating_sub(h_cost as Minute),
-                        );
-                        best = best.max(sub + (minute.saturating_sub(1)) * e_flow);
-                    }
-                }
-            }
-        }
-        (true, false) => {
-            for &(e_child, e_cost) in graph.get_next(&e) {
-                let res = dfs_2::<GREEDY>(
-                    cache,
-                    graph,
-                    h,
-                    e_child,
-                    opened | h_valve,
-                    minute.saturating_sub(e_cost as Minute),
-                );
-                best = best.max(res + minute.saturating_sub(1) * h_flow);
-            }
-        }
-        (false, true) => {
-            for &(h_child, h_cost) in graph.get_next(&h) {
-                let res = dfs_2::<GREEDY>(
-                    cache,
-                    graph,
-                    h_child,
-                    e,
-                    opened | e_valve,
-                    minute.saturating_sub(h_cost as Minute),
-                );
-                best = best.max(res + minute.saturating_sub(1) * e_flow);
-            }
-        }
-        (false, false) => {
-            if GREEDY {
-                for &(h_child, h_cost) in graph.get_next(&h) {
-                    for &(e_child, e_cost) in graph.get_next(&e) {
-                        let res = dfs_2::<GREEDY>(
-                            cache,
-                            graph,
-                            h_child,
-                            e_child,
-                            opened,
-                            minute.saturating_sub((h_cost + e_cost) as Minute),
-                        );
-                        best = best.max(res);
-                    }
-                }
-            }
-        }
+    if (h.1 == 0 && e.1 == 0) || minute == 0 || graph.all_open(&opened) {
+        return 0;
     }
 
-    if !GREEDY {
-        for &(h_child, h_cost) in graph.get_next(&h) {
-            for &(e_child, e_cost) in graph.get_next(&e) {
-                let res = dfs_2::<GREEDY>(
-                    cache,
-                    graph,
-                    h_child,
-                    e_child,
-                    opened,
-                    minute.saturating_sub((h_cost + e_cost) as Minute),
-                );
-                best = best.max(res);
-            }
+    let h_valve = *graph.get_valve(&h.0);
+    let h_flow = graph.get_flow(&h.0) as Score;
+
+    let e_valve = *graph.get_valve(&e.0);
+    let e_flow = graph.get_flow(&e.0) as Score;
+
+    let mut opened = opened;
+    let mut score = 0;
+
+    let mut h = h;
+    let mut e = e;
+
+    if h.1 == minute && h_valve & opened == 0 {
+        opened |= h_valve;
+        h.1 -= 1;
+        score += h.1 * h_flow;
+    }
+    if e.1 == minute && e_valve & opened == 0 {
+        opened |= e_valve;
+        e.1 -= 1;
+        score += e.1 * e_flow;
+    }
+
+    // Both walking to a valve...
+    if h.1 < minute && e.1 < minute {
+        let min_min = h.1.abs_diff(minute).min(e.1.abs_diff(minute));
+        let new_min = minute - (min_min);
+        return score + dfs_2(cache, graph, h, e, opened, new_min);
+    }
+
+    let get_valid = |node: (Node, Minute)| -> Vec<(Node, Minute)> {
+        // Is at designates target
+        if node.1 == minute {
+            graph
+                .nodes()
+                .iter()
+                .filter(|n| graph.get_valve(n) & opened == 0 && **n != node.0)
+                .flat_map(|n| {
+                    let time = node.1.checked_sub(graph.distance(&node.0, n) as Minute);
+                    time.map(|c| (*n, c))
+                })
+                .filter(|n| node.1 > n.1)
+                .collect::<Vec<_>>()
+        } else {
+            // Still walking to its target node... wait
+            vec![(node.0.clone(), node.1.clone())]
+        }
+    };
+
+    let h_next = get_valid(h);
+    let e_next = get_valid(e);
+
+    // if h_next.is_empty() && e_next.is_empty() {
+    //     return score;
+    // }
+
+    let mut best = Score::MIN;
+    for next_h in &h_next {
+        for next_e in &e_next {
+            let res = dfs_2(
+                cache,
+                graph,
+                *next_h,
+                *next_e,
+                opened,
+                // A Minute passes...
+                minute.saturating_sub(1),
+            );
+            best = best.max(res + score);
         }
     }
 
@@ -298,11 +209,34 @@ fn dfs<const GREEDY: bool>(
 }
 
 trait Graph {
+    // fn from_empty(&self) -> &[(Node, u8)];
+    fn nodes(&self) -> Vec<Node>;
+
     fn get_next(&self, node: &Node) -> &[(Node, u8)];
     fn get_flow(&self, node: &Node) -> u8;
     fn get_valve(&self, node: &Node) -> &Valve;
 
     fn all_open(&self, mask: &ValveMask) -> bool;
+
+    fn dist(&self, f: &Node, t: &Node, path: &mut Vec<Node>) -> u16 {
+        path.push(*f);
+        let mut res = 0;
+        for (node, d) in self.get_next(f) {
+            if node == t {
+                return *d as u16;
+            }
+            if path.contains(node) {
+                continue;
+            }
+            path.push(*node);
+            res = self.dist(node, t, path).saturating_add(*d as u16);
+            path.pop();
+        }
+        res
+    }
+    fn distance(&self, from: &Node, to: &Node) -> u16 {
+        self.dist(from, to, &mut Vec::new())
+    }
 }
 
 #[derive(Debug)]
@@ -315,46 +249,71 @@ struct PruneGraph {
 }
 
 impl PruneGraph {
+    fn prune_from(
+        graph: &GraphRaw,
+        source: Node,
+        from: Node,
+        nodes: &[(Node, u8)],
+        cost: u8,
+    ) -> Vec<(Node, u8)> {
+        let mut res = Vec::new();
+        for (node, _) in nodes {
+            if graph.get_flow(node) == 0 {
+                let mut next = graph.get_next(node).to_vec();
+                next.retain(|n| n.0 != source && n.0 != from);
+                if next.is_empty() {
+                    break;
+                }
+                res.extend(PruneGraph::prune_from(
+                    graph,
+                    source,
+                    *node,
+                    &next,
+                    cost + 1,
+                ));
+            } else {
+                res.push((*node, cost));
+            }
+        }
+        res
+    }
+
     fn from_graph(graph: GraphRaw) -> Self {
         let mut map = HashMap::new();
 
-        fn dive(
-            graph: &GraphRaw,
-            source: Node,
-            from: Node,
-            nodes: &[(Node, u8)],
-            cost: u8,
-        ) -> Vec<(Node, u8)> {
-            let mut res = Vec::new();
-            for (node, _) in nodes {
-                if graph.get_flow(node) == 0 {
-                    let mut next = graph.get_next(node).to_vec();
-                    next.retain(|n| n.0 != source && n.0 != from);
-                    if next.is_empty() {
-                        break;
-                    }
-                    res.extend(dive(graph, source, *node, &next, cost + 1));
-                } else {
-                    res.push((*node, cost));
-                }
-            }
-            res
+        let non_zero = graph.nodes.iter().filter(|n| graph.get_flow(n.0) > 0);
+        for (node, nodes) in non_zero {
+            map.insert(
+                *node,
+                PruneGraph::prune_from(&graph, *node, *node, nodes, 1),
+            );
         }
 
-        for (node, nodes) in &graph.nodes {
-            map.insert(*node, dive(&graph, *node, *node, nodes, 1));
-        }
+        let valves = graph
+            .valves
+            .into_iter()
+            .filter(|e| map.keys().any(|k| *k == e.0))
+            .collect::<HashMap<_, _>>();
+        let flows = graph
+            .flows
+            .into_iter()
+            .filter(|(_, f)| *f > 0)
+            .collect::<HashMap<_, _>>();
 
         Self {
-            valves: graph.valves,
+            valves,
             nodes: map,
-            flows: graph.flows,
+            flows,
             _all_valves_mask: graph._all_valves_mask,
         }
     }
 }
 
 impl Graph for PruneGraph {
+    fn nodes(&self) -> Vec<Node> {
+        self.nodes.keys().cloned().collect::<Vec<_>>()
+    }
+
     fn get_next(&self, node: &Node) -> &[(Node, u8)] {
         &self.nodes.get(node).unwrap()[..]
     }
@@ -404,6 +363,10 @@ impl GraphRaw {
 }
 
 impl Graph for GraphRaw {
+    fn nodes(&self) -> Vec<Node> {
+        self.nodes.keys().cloned().collect::<Vec<_>>()
+    }
+
     fn get_next(&self, node: &Node) -> &[(Node, u8)] {
         &self.nodes.get(node).unwrap()[..]
     }
@@ -418,32 +381,6 @@ impl Graph for GraphRaw {
 
     fn all_open(&self, mask: &ValveMask) -> bool {
         self._all_valves_mask == *mask & self._all_valves_mask
-    }
-}
-
-impl Display for GraphRaw {
-    fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
-        let valves = self
-            .valves
-            .iter()
-            .map(|(k, v)| format!("\n\t\t{k} = {v:0>64b}"))
-            .collect::<String>();
-        let nodes = self
-            .nodes
-            .iter()
-            .map(|(k, v)| format!("\n\t\t{k} = {v:?}"))
-            .collect::<String>();
-        let flows = self
-            .flows
-            .iter()
-            .map(|(k, v)| format!("\n\t\t{k:0>64b} => {v}"))
-            .collect::<String>();
-
-        write!(
-            f,
-            "Graph {{\n\tValves{}\n\tNodes:{}\n\tFlows:{},\n\tA-Open:\t{:0>64b}\n}}",
-            valves, nodes, flows, self._all_valves_mask
-        )
     }
 }
 
@@ -517,10 +454,21 @@ mod tests {
     fn test_day16_part1() {
         let (graph, valves) = parse_input(TEST);
         let graph = transform((graph, valves));
+        let init = PruneGraph::prune_from(&graph, 0, 0, graph.get_next(&0), 1);
         let graph = PruneGraph::from_graph(graph);
 
-        let mut cache = HashMap::new();
-        let best = dfs::<false>(&mut cache, &graph, 0, 0, 30, 0);
+        let mut best = Score::MIN;
+        for (start, cost) in init {
+            let res = dfs::<false>(
+                &mut HashMap::new(),
+                &graph,
+                start,
+                0,
+                30 - cost as Minute,
+                0,
+            );
+            best = best.max(res);
+        }
 
         assert_eq!(1651, best);
     }
@@ -529,11 +477,35 @@ mod tests {
     fn test_day16_part2() {
         let (graph, valves) = parse_input(TEST);
         let graph = transform((graph, valves));
+
+        let initial = PruneGraph::prune_from(&graph, 0, 0, graph.get_next(&0), 1);
+
         let graph = PruneGraph::from_graph(graph);
 
-        let best = dfs_2::<false>(&mut HashMap::new(), &graph, 0, 0, 0, 26);
-        let bestg = dfs_2::<true>(&mut HashMap::new(), &graph, 0, 0, 0, 26);
-        println!("best lazy {best}, best greedy {bestg}");
+        let mut best = Score::MIN;
+        for i in 0..initial.len() {
+            let h = initial[i];
+            let e = initial[(i + 1) % initial.len()];
+            let res = dfs_2(
+                &mut HashMap::new(),
+                &graph,
+                (h.0, 26 - h.1 as Minute),
+                (e.0, 26 - e.1 as Minute),
+                0,
+                26,
+            );
+            println!(
+                "Result at (initial) ({}, {}) => {} cmp {best}",
+                initial[i].0,
+                initial[(i + 1) % initial.len()].0,
+                res
+            );
+            best = best.max(res);
+        }
+
+        // let best = dfs_3::<false>(&mut HashMap::new(), &graph, (0, 26), (0, 26), 0, 26);
+        // let bestg = dfs_3::<true>(&mut HashMap::new(), &graph, (0, 26), (0, 26), 0, 26);
+        // println!("best lazy {best}, best greedy {bestg}");
 
         assert_eq!(1707, best);
     }
