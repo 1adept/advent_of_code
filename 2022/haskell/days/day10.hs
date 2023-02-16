6e93e047262457798a081a41f0d2924f0c5bd930
1adept <69433209+1adept@users.noreply.github.com>
2023-02-16 18:50:21 +0100

    haskell day10 part2

--- 2022/haskell/days/day10.hs
+++ 2022/haskell/days/day10.hs
@@ -1,12 +1,7 @@
 import AoC
 
-import Data.List
-import GHC.Read (readField)
-import Debug.Trace (trace)
-import Text.Printf (printf)
-
 type Instruction = Maybe Int
-type State = (Int, Int)         -- (Register value, Cycle, Cycle behind)
+type State       = (Int, Int)   -- (Cycle, Register value)
 
 day :: Int
 day = 10
@@ -31,11 +26,39 @@ main = do
     print $ "Part1 example: " ++ show (solve1 e)
     print $ "Part1   input: " ++ show (solve1 i)
 
+    -- Doesnt work with this uncommented
+    -- putStrLn "Part2 example:"
+    -- printCRT $ solve2 e
+
+    putStrLn "Part2 input:"
+    printCRT $ solve2 i
+
+    where
+        printCRT :: [String] -> IO ()
+        printCRT []           = return ()
+        printCRT (first:rest) = do
+            putStrLn first
+            printCRT rest
+
 solve1 :: [Instruction] -> Int
 solve1 = sum 
         . map (uncurry (*))
         . filter (\(cycle, _) -> cycle >= 20 && (cycle - 20) `mod` 40 == 0) 
         . exec
+
+solve2 :: [Instruction] -> [String]
+solve2 instructions = breakLines [] $ spritify instructions
+    where
+        spritify :: [Instruction] -> String
+        spritify = 
+                map (
+                    (\b -> if b then '#' else '.')
+                    . (\(c, r) -> abs (((c - 1) `mod` 40) - r) <= 1)
+                )
+                . exec
+        breakLines :: [String] -> String -> [String]
+        breakLines res []  = res
+        breakLines res src = breakLines (res ++ [take 40 src]) (drop 40 src)
 
 exec :: [Instruction] -> [State]
 exec instructions = exec' instructions [(1, 1)] -- Starting cycle and register value are 1 each
